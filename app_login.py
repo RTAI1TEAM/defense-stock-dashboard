343b4db9fe858b56051e83114372d7a63ff5cffe
shkong <170008313+honghonghee@users.noreply.github.com>
2026-03-09 15:57:01 +0900

    feat: 로그인/회원가입 env 보안 설정

--- app_login.py
+++ app_login.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.secret_key = 'aiquant2024'
+app.secret_key = os.getenv('SECRET_KEY')
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'angelina11!!'
-app.config['MYSQL_DB'] = 'ai_quant'
-app.config['MYSQL_PORT'] = 3307
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
 
 mysql = MySQL(app)
 
