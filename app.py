2adb657bf13ca3fdeb3dd90df7637f78c5104466
jiwon <chabr0905@gmail.com>
2026-03-09 16:39:49 +0900

    feat: 뉴스페이지  추가

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from datetime import datetime
 from flask import Flask, render_template, request, jsonify
 import requests
 from routes.rank import rank_bp
+from routes.news import news_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(rank_bp)
+app.register_blueprint(news_bp)
 
 @app.route("/")
 def index():
