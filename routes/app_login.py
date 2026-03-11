b4fae27457b1704df52d0cfea91aedd9bb0b4afd
jiwon <chabr0905@gmail.com>
2026-03-11 13:06:51 +0900

    feat: 프로필 아바타추가,랭크 아바타 추가,네비바수정, 테스트더미수정

--- routes/app_login.py
+++ routes/app_login.py
@@ -37,6 +37,7 @@ def login():
 
     if user and bcrypt.checkpw(password.encode('utf-8'), user['password_hash'].encode('utf-8')):
         session['nickname'] = user['nickname']
+        session['avatar'] = user.get('avatar') or '🧑‍💼'
         return redirect(url_for("index"))
 
     return render_template(
